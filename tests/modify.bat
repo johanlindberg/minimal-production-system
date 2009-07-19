3b1c47d38c95f266145560636b23169997a65d1e
Johan Lindberg <johan@pulp.se>
2009-07-19 23:41:38 +0200

    Tests for modify-facts.

--- tests/modify.bat
+++ tests/modify.bat
@@ -1,6 +1,31 @@
-;; Tests for MPS:MODIFY-FACTS
-(clear)
-(assert-facts #S(foo :bar 1) #S(foo :bar 2))
-(modify-facts ((1 (bar 3))))
-(facts)
-
+;; doctests for modify-facts functionality
+>> (progn
+     (unless (equal (package-name *package*) "MPS")
+       (in-package :mps))
+     NIL)
+NIL
+>> (clear)
+T
+>> (defstruct foo bar)
+FOO
+>> (defparameter *fact* #S(foo :bar 1))
+*FACT*
+>> *fact*
+(#S(FOO :BAR 1))
+>> (assert-facts *fact*)
+-> |=> FACT: F-1 #S(FOO :BAR 1)|
+1
+>> (modify-facts (1 (bar 2))) ; F-1
+1
+>> (facts)
+(#S(FOO :BAR 2))
+>> *fact*
+(#S(FOO :BAR 2))
+>> (modify-facts (*fact* (bar 3)))
+1
+>> (facts)
+(#S(FOO :BAR 3))
+>> (modify-facts (#S(foo :bar 3) (bar 4)))
+1
+>> (facts)
+(#S(FOO :BAR 4))
