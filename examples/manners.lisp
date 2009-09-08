4a2a0cfe88463067c7b7a773200d753cffce8143
Johan Lindberg <johan@pulp.se>
2009-09-08 21:09:21 +0200

    Restored the order of deffacts to match CLIPS.

--- examples/manners.lisp
+++ examples/manners.lisp
@@ -43,19 +43,19 @@
 ;;; Guests
 
 (deffacts guests
-  #S(context :state start)
-  #S(count :c 1)
-  #S(last_seat :seat 4)
-  #S(guest :name n4 :sex f :hobby h3)
-  #S(guest :name n4 :sex f :hobby h2)
-  #S(guest :name n4 :sex f :hobby h1)
-  #S(guest :name n3 :sex f :hobby h2)
-  #S(guest :name n3 :sex f :hobby h3)
-  #S(guest :name n2 :sex m :hobby h3)
-  #S(guest :name n2 :sex m :hobby h2)
-  #S(guest :name n2 :sex m :hobby h1)
+  #S(guest :name n1 :sex m :hobby h3)
   #S(guest :name n1 :sex m :hobby h2)
-  #S(guest :name n1 :sex m :hobby h3))
+  #S(guest :name n2 :sex m :hobby h1)
+  #S(guest :name n2 :sex m :hobby h2)
+  #S(guest :name n2 :sex m :hobby h3)
+  #S(guest :name n3 :sex f :hobby h3)
+  #S(guest :name n3 :sex f :hobby h2)
+  #S(guest :name n4 :sex f :hobby h1)
+  #S(guest :name n4 :sex f :hobby h2)
+  #S(guest :name n4 :sex f :hobby h3)
+  #S(last_seat :seat 4)
+  #S(count :c 1)
+  #S(context :state start))
 
 ;;; Rules
 
